78d1d75b0b4ef3e6db3713be32ed64707addfd7a
Cherryleafroad <cherryleafroad@gmail.com>
2020-12-09 18:59:32 -0800

    Experimental new version - dynamically switches between map only loading / list only (limited screen size feature of zillow) - hides map markers
    Not net functional however

--- content.js
+++ content.js
@@ -1,91 +1,144 @@
-// checks if there's a map on the page. if so, we're looking for houses
-let t = document.querySelector("div#search-page-map-container");
-extensionLoaded = false;
+// check existence of house list
+let l = document.querySelectorAll("ul.photo-cards > li") !== null;
+let extensionLoaded = false;
+let mapLoaded = false;
+let listLoaded = false;
 
-if (t !== null) {
+if (l) {
     extensionLoaded = true;
-    // initial load
-    app();
 
+    mapLoaded = document.querySelector("div#search-page-map-container") !== null;
+    listLoaded = document.querySelectorAll("ul.photo-cards") !== null;
+    
+    // initial load
+    if (mapLoaded) {
+        document.querySelector("div#search-page-map-container").arrive("div.BulkPropertyMapMarker:not(.high-zoom)", { onceOnly: true }, () => {
+            app();
+        });
+    } else {
+        app();
+    }
+    
+    // keep track of map loaded states to ensure we ignore or hide markers
+    document.querySelector("div.search-page-container").arrive("div#search-page-map-container", () => {
+        mapLoaded = true;
+        document.querySelector("div#search-page-map-container").arrive("div.BulkPropertyMapMarker:not(.high-zoom)", { onceOnly: true }, () => {
+            // re-run app to hide markers
+            app();
+        });
+    });
+    document.querySelector("div.search-page-container").leave("div#search-page-map-container", () => {
+        mapLoaded = false;
+    });
+
+    
+    document.querySelector("div.search-page-container").arrive("div#grid-search-results", () => {
+        listLoaded = true;
+        // re-run app to ensure correct things hidden/showed
+        app();
+        
+        // reconnect signal because it was lost
+        document.querySelector("div#grid-search-results").leave("div.list-loading-message-cover", () => {
+            app();
+        });
+    });
+
+    document.querySelector("div.search-page-container").leave("div#grid-search-results", () => {
+        listLoaded = false;
+    });
+    
+    // connect original list loading finished signal
     document.querySelector("div#grid-search-results").leave("div.list-loading-message-cover", () => {
         app();
     });
 }
 
 function app() {
-  const thumbnails = document.querySelectorAll("ul.photo-cards > li");
+    // make sure array is empty first
+    houseList = [];
 
-  thumbnails.forEach(thumbnail => {
-    // it's an ad, skip it
-    if (thumbnail.lastChild.id === "nav-ad-container") {
-        // also hide it
-        thumbnail.setAttribute("style", "display:none;");
-        return;
-    }
+    const thumbnails = document.querySelectorAll("ul.photo-cards > li");
+    
+    mapMarkers = mapLoaded ? getMapMarkers() : new Array(thumbnails.length);
 
-    const houseId = thumbnail.lastChild.id;
+    thumbnails.forEach((thumbnail, i) => {
+        // it's an ad, skip it
+        if (thumbnail.lastChild.id === "nav-ad-container") {
+            // also hide it
+            thumbnail.setAttribute("style", "display:none;");
+            return;
+        }
 
-    // remove all hidden houses from DOM
-    storageGet(houseIdObj => {
-      if (Object.keys(houseIdObj).includes(houseId)) {
-        console.log("Hid houseId " + houseId);
-        removeHouseFromDOM(thumbnail);
-      }
+        const houseId = thumbnail.lastChild.id;
+
+        // remove all hidden houses from DOM
+        storageGet(houseIdObj => {
+            if (Object.keys(houseIdObj).includes(houseId)) {
+                console.log("Hid houseId " + houseId);
+                removeHouseFromDOM(thumbnail, mapMarkers[i]);
+            }
+        });
+
+        // add hidden button to visible houses
+        if (thumbnail.id != "hideButton") {
+            thumbnail.setAttribute("id", "hideButton");
+            // add spacing on favorite button
+            // this is the SPAN of the heart
+            thumbnail.lastChild.lastChild.firstChild.setAttribute("style", "margin-right: 44px;");
+            // add hide button
+            thumbnail.lastChild.appendChild(createHideButton(houseId, thumbnail, mapMarkers[i]));
+        }
+        
+        thumbnail.lastChild.onclick = () => {  onClickHouse(houseId, thumbnail, mapMarkers[i]); }
     });
 
-    // add hidden button to visible houses
-    if (thumbnail.id != "hideButton") {
-      thumbnail.setAttribute("id", "hideButton");
-      // add spacing on favorite button
-      // this is the SPAN of the heart
-      thumbnail.lastChild.lastChild.firstChild.setAttribute("style", "margin-right: 44px;");
-      // add hide button
-      thumbnail.lastChild.appendChild(createHideButton(houseId, thumbnail));
+    // add a clear hidden results link
+    var header = document.getElementsByClassName("search-page-list-header")[0];
+    if (header.id != "clearButton") {
+        header.setAttribute("id", "clearButton");
+        clearButton = document.createElement("a");
+        clearButton.setAttribute("id", "clearButtonA");
+        clearButton.appendChild(document.createTextNode("Clear Hidden Results"));
+        clearButton.onclick = () => {
+            unhideHouses();
+        }
+        header.appendChild(clearButton);
+        updateHiddenCount();
     }
-    
-    thumbnail.lastChild.onclick = () => {  onClickHouse(houseId, thumbnail); }
-  });
-  
-  // add a clear hidden results link
-  var header = document.getElementsByClassName("search-page-list-header")[0];
-  if (header.id != "clearButton") {
-      header.setAttribute("id", "clearButton");
-      clearButton = document.createElement("a");
-      clearButton.setAttribute("id", "clearButtonA");
-      clearButton.appendChild(document.createTextNode("Clear Hidden Results"));
-      clearButton.onclick = () => { unhideHouses(); }
-      header.appendChild(clearButton);
-      updateHiddenCount();
-  }
 }
 
 function unhideHouses() {
     chrome.storage.sync.clear();
     updateHiddenCount();
-    
-  const thumbnails = document.querySelectorAll("ul.photo-cards > li");
 
-  thumbnails.forEach(thumbnail => {
-    // it's an ad, skip it
-    if (thumbnail.lastChild.id === "nav-ad-container")
-        return;
-
-    // unhide it
-    if (thumbnail.style.display === "none") {
-        thumbnail.removeAttribute("style");
+    const thumbnails = document.querySelectorAll("ul.photo-cards > li");
+    if (mapLoaded) {
+        const mapMarkers = getMapMarkers();
     }
-  });
-}
 
-function onClickHouse(houseId, thumbnail) {
-    var target = document.querySelector("div#home-detail-lightbox-container");
+    thumbnails.forEach((thumbnail, i) => {
+        // it's an ad, skip it
+        if (thumbnail.lastChild.id === "nav-ad-container")
+            return;
 
-    target.arrive("button.sc-AxhCb.eSwYtm.hdp__sc-1tf5ijk-22.iMJDnd", { onceOnly: true }, () => {
-        createDetailHideButton(houseId, thumbnail);
+        // unhide it
+        if (thumbnail.style.display === "none") {
+            thumbnail.style.display = "";
+            if (mapLoaded)
+                mapMarkers[i].style.display = "";
+        }
     });
 }
 
-function createDetailHideButton(houseId, thumbnail) {
+function onClickHouse(houseId, thumbnail, marker) {
+    var target = document.querySelector("div#home-detail-lightbox-container");
+
+    target.arrive("button.sc-AxhCb.eSwYtm.hdp__sc-1tf5ijk-22.iMJDnd", { onceOnly: true }, () => {
+        createDetailHideButton(houseId, thumbnail, marker);
+    });
+}
+
+function createDetailHideButton(houseId, thumbnail, marker) {
     var ul = document.querySelector("ul.hdp__sc-1tf5ijk-9.fbBaJs");
 
     var li = document.createElement("li");
@@ -95,7 +148,7 @@ function createDetailHideButton(houseId, thumbnail) {
     button.setAttribute("aria-pressed", "false");
     button.setAttribute("class", "sc-AxhCb eSwYtm hdp__sc-1tf5ijk-22 iMJDnd");
     button.onclick = () => {
-        onClick(houseId, thumbnail);
+        onClick(houseId, thumbnail, marker);
         document.querySelector("button.ds-close-lightbox-icon.hc-back-to-list").click();
     };
     li.appendChild(button);
@@ -117,13 +170,15 @@ function createDetailHideButton(houseId, thumbnail) {
   Given the dom object of the actions bar for each house thumbnail,
   remove the entire thumbnail for that given house
 */
-function removeHouseFromDOM(thumbnailDOM) {
+function removeHouseFromDOM(thumbnailDOM, markerDOM) {
     // turns out removing from DOM causes sidebar to disappear
     //const houseList = thumbnailDOM.parentNode;
     //houseList.removeChild(thumbnailDOM);
-    
+
     // but we can hide it instead
-    thumbnailDOM.setAttribute("style", "display:none;");
+    thumbnailDOM.style.display = "none";
+    if (mapLoaded)
+        markerDOM.style.display = "none";
 }
 
 function updateHiddenCount(increase = false) {
@@ -134,41 +189,56 @@ function updateHiddenCount(increase = false) {
             count++;
 
         // update badge
-        chrome.runtime.sendMessage({ numHiddenHouses: count });
+        chrome.runtime.sendMessage({
+            numHiddenHouses: count
+        });
 
         elem = document.getElementsByClassName("search-page-list-header")[0].children.clearButtonA;
         elem.textContent = "Clear Hidden Results (" + count + ")";
     });
 }
 
+function getMapMarkers() {
+    // Map Markers are reversed in relation to the hosue list. So we append them in reverse order to get the same ordering
+    const mapMarkersD = document.querySelector("div.BulkPropertyMapMarker:not(.high-zoom)");
+
+    mapMarkers = [];
+    var i = mapMarkersD.childNodes.length;
+    while (i--)
+        mapMarkers.push(mapMarkersD.childNodes[i]);
+        
+    return mapMarkers;
+}
+
 /*
   Create a hide house button
 */
-function createHideButton(houseId, thumbnailDOM) {
-  hideButton = document.createElement("button");
-  hideButton.setAttribute("class", "list-card-save");
-  hideButton.setAttribute("type", "button");
-  
-  span = document.createElement("span");
-  span.setAttribute("class", "list-card-save-content");
-  span.setAttribute("style", "padding-top:12px;");
-  hideButton.appendChild(span);
-  
-  link = document.createElement("a");
-  link.appendChild(document.createTextNode("❌"));
-  link.onclick = (e) => {
+function createHideButton(houseId, thumbnailDOM, markerDOM) {
+    hideButton = document.createElement("button");
+    hideButton.setAttribute("class", "list-card-save");
+    hideButton.setAttribute("type", "button");
+
+    span = document.createElement("span");
+    span.setAttribute("class", "list-card-save-content");
+    span.setAttribute("style", "padding-top:12px;");
+    hideButton.appendChild(span);
+
+    link = document.createElement("a");
+    link.appendChild(document.createTextNode("❌"));
+    link.onclick = (e) => {
         // don't double click element underneath
         e.stopPropagation();
-        onClick(houseId, thumbnailDOM);
-  };
-  span.appendChild(link);
-  return hideButton;
+        // don't double click element underneath
+        onClick(houseId, thumbnailDOM, markerDOM);
+    };
+    span.appendChild(link);
+    return hideButton;
 }
 
-function onClick(houseId, thumbnailDOM) {
-  storageSet(houseId, true);
-  removeHouseFromDOM(thumbnailDOM);
-  updateHiddenCount(true);
+function onClick(houseId, thumbnailDOM, markerDOM) {
+    storageSet(houseId, true);
+    removeHouseFromDOM(thumbnailDOM, markerDOM);
+    updateHiddenCount(true);
 }
 
 chrome.runtime.onMessage.addListener(
@@ -183,27 +253,27 @@ chrome.runtime.onMessage.addListener(
   Fetch data from chrome storage
 */
 function storageGet(callback) {
-  chrome.storage.sync.get(["zillowHouseHide"], result => {
-    // key not yet set
-    if (Object.keys(result).length === 0) {
-        callback({});
-        return;
-    }
+    chrome.storage.sync.get(["zillowHouseHide"], result => {
+        // key not yet set
+        if (Object.keys(result).length === 0) {
+            callback({});
+            return;
+        }
 
-    callback(result.zillowHouseHide);
-  });
+        callback(result.zillowHouseHide);
+    });
 }
 
 /*
   Update data in chrome storage
 */
 function storageSet(key, value) {
-  chrome.storage.sync.get(["zillowHouseHide"], result => {
-    chrome.storage.sync.set({
-      zillowHouseHide: {
-        ...result.zillowHouseHide,
-        [key]: value
-      }
+    chrome.storage.sync.get(["zillowHouseHide"], result => {
+        chrome.storage.sync.set({
+            zillowHouseHide: {
+                ...result.zillowHouseHide,
+                [key]: value
+            }
+        });
     });
-  });
 }
